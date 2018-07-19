a6a89a968f938366bf3b20e4bba126d51c231371
zhijia,.zhang <jiahut@gmail.com>
2018-07-19 20:46:12 +0800

    add the age_gt

--- src/main/kotlin/com/goodjob/StaffController.kt
+++ src/main/kotlin/com/goodjob/StaffController.kt
@@ -2,8 +2,8 @@ package com.goodjob
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam
 
 @RestController
 class StaffController(private val staffRepository: StaffRepository) {
@@ -11,7 +11,10 @@ class StaffController(private val staffRepository: StaffRepository) {
     @GetMapping("/staffs")
     fun findAll() = staffRepository.findAll()
 
-    @GetMapping("/staff/{lastName}")
-    fun findByLastName(@PathVariable lastName: String) = staffRepository.findByLastName(lastName)
+    @GetMapping("/staff")
+    fun findByLastName(@RequestParam("lastName") lastName: String) = staffRepository.findByLastName(lastName)
+
+    @GetMapping("/staff/age_gt/{age}")
+    fun findByAgeGt(@PathVariable age: Int) = staffRepository.findByAgeGreaterThen(age)
 
 }
--- src/main/kotlin/com/goodjob/StaffRepository.kt
+++ src/main/kotlin/com/goodjob/StaffRepository.kt
@@ -1,7 +1,10 @@
 package com.goodjob
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface StaffRepository : CrudRepository<Staff, Long> {
     fun findByLastName(lastName: String): Iterable<Staff>
+    @Query("select firstName,lastName,age,id  from Staff s where s.age > ?1")
+    fun findByAgeGreaterThen(age: Int): Iterable<Staff>
 }
