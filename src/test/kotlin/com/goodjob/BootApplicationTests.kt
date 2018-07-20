f028bd8d8c847d6d7937bb0f0e877f74cd7d3f2c
zhijia,.zhang <jiahut@gmail.com>
2018-07-20 11:29:52 +0800

    add the datajpa test

--- src/test/kotlin/com/goodjob/BootApplicationTests.kt
+++ src/test/kotlin/com/goodjob/BootApplicationTests.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForObject
@@ -11,14 +12,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class BootApplicationTests(@Autowired private val restTemplate: TestRestTemplate) {
+
+class BootApplicationTests(
+        @Autowired private val restTemplate: TestRestTemplate) {
 
     @Test
     fun findAll() {
-
         val content = """[{"firstName":"zhang","lastName":"zhijia","age":29,"id":1},{"firstName":"bei","lastName":"bei","age":28,"id":2}]"""
         assertEquals(content, restTemplate.getForObject<String>("/staffs"))
-
     }
 
 }
---
+++ src/test/kotlin/com/goodjob/StaffRepositoryTests.kt
@@ -0,0 +1,23 @@
+package com.goodjob
+
+import org.assertj.core.util.Lists
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.util.Assert
+
+
+@ExtendWith(SpringExtension::class)
+@DataJpaTest
+class StaffRepositoryTests(
+        @Autowired private val staffRepository: StaffRepository) {
+
+    @Test
+    fun findByLastName() {
+        val staff = staffRepository.findByLastName("zhijia")
+        var list = Lists.newArrayList(staff)
+        Assert.isTrue(1 == list.size, "has not fond")
+    }
+}
